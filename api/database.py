2b111d5ceb5dd515671a1d73b0ed64e595896941
elaine123tv <104650000+elaine123tv@users.noreply.github.com>
2025-04-23 00:50:08 +0100

    Update database.py

--- api/database.py
+++ api/database.py
@@ -5,10 +5,11 @@ import boto3
 
 Base = declarative_base()
 ssm = boto3.client('ssm', region_name='eu-west-2')
-data = ssm.get_parameter(
-            Name='/dev/api/db-url',
-            WithDecryption=True
-        )
+try:
+    data = ssm.get_parameter(Name='/dev/api/db-url', WithDecryption=True)
+    URL_DATABASE = data['Parameter']['Value']
+except ssm.exceptions.ParameterNotFound:
+    raise ValueError("SSM parameter not found")
 
 URL_DATABASE = data['Parameter']['Value']
 engine = create_engine(URL_DATABASE)
