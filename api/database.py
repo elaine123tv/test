6596ae81c12d28095db1f09ef8650638f23ee6a1
elaine123tv <104650000+elaine123tv@users.noreply.github.com>
2025-04-23 01:50:20 +0100

    Update database.py

--- api/database.py
+++ api/database.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import boto3
 
+# base (foundational) class that allows us to define database models by mapping Python classes to database tables.
 Base = declarative_base()
-ssm = boto3.client('ssm', region_name='eu-west-2')
+ssm = boto3.client('ssm', region_name='eu-west-2')  # set up connection to AWS Parameter Store for getting database password
 try:
-    data = ssm.get_parameter(Name='/dev/api/db-url', WithDecryption=True)
-    URL_DATABASE = data['Parameter']['Value']
+    data = ssm.get_parameter(Name='/dev/api/db-url', WithDecryption=True) 
+    URL_DATABASE = data['Parameter']['Value'] # get the database connection string from AWS Parameter Store
 except ssm.exceptions.ParameterNotFound:
     raise ValueError("SSM parameter not found")
 
-engine = create_engine(URL_DATABASE)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(URL_DATABASE)  # create engine that connects to database 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # create factory for making database sessions (safe temporary connections)
